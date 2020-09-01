694cd67411b2a6894cb97c25016a50a7be44c46d
Edison Ponari <eponari19@epoka.edu.al>
2020-09-01 08:47:36 +0200

    Initial Commit

--- CPP/Symmetric array.cpp
+++ CPP/Symmetric array.cpp
@@ -1,4 +1,5 @@
 /*
+
     Symmetric Matrix
 
     You are given a square matrix with a size. Then you are given size*size int numbers.
@@ -12,7 +13,25 @@
 
     Output:
     Yes
+
 */
+
+/*
+
+    Solution:
+    We know that a matrix is symmetric if each element regarding the main diagonal is the same.
+    Example: matrix[1][2] should be equal to matrix[2][1]
+    So we only need to check the upper triangle of a matrix to verify if it is symmetric or not.
+    
+    Upper triangle marked with "*":
+
+    - * * *
+    - - * *
+    - - - *
+    - - - -
+
+*/
+
 
 #include <iostream>
 using namespace std;
@@ -21,18 +40,22 @@ int main(){
   int size;
   cin>>size;
   int matrix[size][size];
+  //get the 2D array as input
   for(int i=0;i<size;i++){
     for(int j=0;j<size;j++){
       cin>>matrix[i][j];
     }
   }
+
   for(int i=0;i<size;i++){
-    for(int j=0;j<size;j++){
+    //the second loop starts from i+1 since we only need to chech only the upper triangle
+    for(int j=i+1;j<size;j++){
       if(matrix[i][j]!=matrix[j][i]){
         cout<<"No";
         return 0;
       }
     }
   }
+
   cout<<"Yes";
 }
---
+++ Python/FindPrimes.py
@@ -0,0 +1,44 @@
+"""
+
+  Find Prime Numbers
+  In a certain range, find the number of prime number
+
+  Test case:
+  10 20
+
+  Output:
+  4
+
+"""
+
+"""
+
+    Solution:
+    Use the idea of Sieve of Eratosthenes(https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes).
+    While we find Primes, make sure to store them and delete their multiples as we seen it in the range.
+    In the end use a list comprehansion comprehension to target the primes within our range.
+
+"""
+
+#The only even prime number is going to be our starting point
+primes=[2]
+
+def findPrimes(lowerLimit,upperlimit):
+  #we know that primes expect 2 are all odd, so we start from 3 until upperLimit and add 2 each itteration: 3,5,7,9...
+  for number in range(3,upperlimit+1,2):
+    for prime in primes:
+      #according to number theory,a non-prime number should have a factor smaller or equal to its squared root.  
+      if prime>number**(0.5):
+        primes.append(number)
+        break
+      #if it is divisable then it isn't prime and we need to move on
+      if number%prime==0:
+        break
+  desiredPrimes=[x for x in primes if x>=lowerLimit and x<=upperlimit]
+  #return the number of primes within limits
+  return len(desiredPrimes)
+
+#list comprehension trick to get multiple inputs from a line in Python 
+lowerLimit,upperLimit=[int(x) for x in input().split()]
+
+print(findPrimes(lowerLimit,upperLimit))
