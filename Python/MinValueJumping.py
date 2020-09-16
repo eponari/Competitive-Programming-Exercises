3407d39d50a882cd8816ea211297135dc29f1b46
Edison Ponari <eponari19@epoka.edu.al>
2020-09-16 10:10:36 +0200

    Initial Commit

---
+++ Python/MinValueJumping.py
@@ -0,0 +1,18 @@
+"""
+  Find the minimum value taking a path while jumping 1 cell.
+  You are given n numbers. You can take a number or jump over it.Find the minimum path.
+
+  Test case:
+  4
+  2 3 4 5
+
+  Output:
+  6
+
+  Explanation:
+  Get first number(2) and third number(4). Jump over the 4th and finish.
+  
+  Solution:
+  A palindrome is a word that is read the same even if it is writen in backwards. To check this we just have to check if the string is equal to
+  the same string in reverse. Python has a build in function for strings to find the reverse of a string.
+"""
