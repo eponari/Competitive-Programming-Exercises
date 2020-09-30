7a4ff6dc90da93628ff335fbf2374367f59c5eec
Edison Ponari <eponari19@epoka.edu.al>
2020-09-30 17:11:39 +0200

    Initial Commit

--- CPP/N pattern.cpp
+++ CPP/N pattern.cpp
@@ -12,4 +12,39 @@
     N  NN
     N   N
 
+    Solution:
+    This shape has 3 components:
+    -first column.
+    -last column.
+    -diagonal.
+    Fill these 3 with "N" and everything else should be " ".
+
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+  char figure[n][n];
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+      //i==0->first column
+      //i==n-1->last column
+      //i==j->diagonal
+      if(j==0||j==n-1||i==j){
+        figure[i][j]='N';
+      }
+      else{
+        figure[i][j]=' ';
+      }
+    }
+  }
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+      cout<<figure[i][j];
+    }
+    cout<<"\n";
+  }
+}
